1a5d3e308bcd8cc977f1051e44fd41de7449b301
Jae Bach Hardie <j@limina.net>
2019-05-05 22:05:14 +0100

    Add emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,25 +2,23 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter MDX Basic',
-    description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@chrisbiscardi',
+    title: 'MaybeSomething',
+    description: 'a blog'
   },
   plugins: [
     {
       resolve: `gatsby-mdx`,
       options: {
-        defaultLayouts: { default: path.resolve('./src/components/layout.js') },
-      },
+        defaultLayouts: { default: path.resolve('./src/components/layout.js') }
+      }
     },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -40,11 +38,17 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
     },
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
+  ]
 }
